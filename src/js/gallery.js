01d72afb34190bbc6ee3159220db50561164b792
imyroslav <imyroslav@ukr.net>
2024-09-25 00:31:04 +0700

    updated-gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -4,23 +4,23 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const images = [
 {
 preview:
-'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+'../public/preview-object-1.jpg',
 original:
-'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+'../public/original-object-1.jpg',
 description: 'Hokkaido Flower',
 },
 {
 preview:
-'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+'../public/preview-object-2.jpg',
 original:
-'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+'../public/original-object-2.jpg',
 description: 'Container Haulage Freight',
 },
 {
 preview:
-'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+'../public/preview-object-3.jpg',
 original:
-'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+'../public/original-object-3.jpg',
 description: 'Aerial Beach View',
 },
 
